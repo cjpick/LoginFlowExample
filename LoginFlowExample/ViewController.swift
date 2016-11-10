45a9d5c5715e0b75c21346ab8263b5b6dc21ebc0
Christopher Pick <cjpick@christophers-macbook-pro.local>
2016-11-09 17:09:17 -0800

    Initial Commit

---
+++ LoginFlowExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginFlowExample
+//
+//  Created by Christopher Pick on 11/9/16.
+//  Copyright © 2016 Christopher Pick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
