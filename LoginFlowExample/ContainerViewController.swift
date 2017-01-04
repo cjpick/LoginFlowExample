5a21e2946f5644faae00b3e1809deae26e331a33
cjpick <cjpick@christophers-macbook-pro.local>
2017-01-04 07:47:18 -0800

    Display in Navigation for logged in.

--- LoginFlowExample/ContainerViewController.swift
+++ LoginFlowExample/ContainerViewController.swift
@@ -18,28 +18,49 @@ class ContainerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateContainer()
+//        updateContainer()
     }
     
     func updateContainer() {
         if loggedIn {
-            if let current = currentViewController {
-                let loggedInController = retrieveViewController(name: "Logged In")
+            if let current = currentViewController, let loggedInController = retrieveViewController(name: "Logged In") as? ViewController {
+                loggedInController.logoutCallBack = logout
                 let navigation = UINavigationController(rootViewController: loggedInController) // Programmatically adding the navigation controller
                 navigation.view.alpha = 0
                 addChildViewController(navigation)
                 transition(from: current, to: navigation, duration: 0.75, options: [.curveEaseInOut], animations: {
                     current.view.alpha = 0
-                    loggedInController.view.alpha = 1
+                    navigation.view.alpha = 1
                 })
+                currentViewController = navigation
+            }
+        } else {
+            if let current = currentViewController, let loginController = retrieveViewController(name: "Login") as? LoginViewController {
+                loginController.loginCallBack = login
+                loginController.view.alpha = 0
+                addChildViewController(loginController)
+                transition(from: current, to: loginController, duration: 0.75, options: [.curveEaseInOut], animations: {
+                    current.view.alpha = 0
+                    loginController.view.alpha = 1
+                })
+                currentViewController = loginController
             }
         }
     }
     
     
     func login() {
-        loggedIn = true
-        updateContainer()
+        if !loggedIn {
+            loggedIn = true
+            updateContainer()
+        }
+    }
+    
+    func logout() {
+        if loggedIn {
+            loggedIn = false
+            updateContainer()
+        }
     }
     
     func retrieveViewController(name:String)->UIViewController {
--- LoginFlowExample/ViewController.swift
+++ LoginFlowExample/ViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var logoutCallBack: (()->Void)?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(logout))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func logout() {
+        logoutCallBack?()
     }
 
 
